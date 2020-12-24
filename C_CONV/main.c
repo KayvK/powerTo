ea7b53d395e1ea200f64f297bc43f4c78089880b
Kayvon <kayvkow@gmail.com>
2020-12-24 02:04:40 -0800

    "Fixed" the long double issue with GCC/MSVC Cleaned up code Added a flush function to prevent the program from losing all brain cells

--- C_CONV/main.c
+++ C_CONV/main.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
-// TODO learn to replace namespaces
-int main(/*int argc, char *argv[]*/){
-    int exponent = 0;
-    double base = 0;
-    printf("enter an exponent and a base: ");
-    scanf("%i %lf", &exponent, &base);
-    printf("%lf", powerTo(exponent, base));
-}
+/* READ ME!!!!!
+Compiling with Windows mixed with GCC screws with things a bit.
+GCC's long double != MSVC's long double.
+And long story short, because of conversion problems, long doubles
+are unusable. End of story.
+*/
+
+int main(int argc, char *argv[]){
+    int exponent = 0; // int replaced with long long int to stop NaN from sqrt overflow with long double.
+    double base = 0; 
+    printf("Enter a base and an exponent: ");
+    fflush(stdout); // I haven't a clue why this is necessary to stop the terribleness
+    scanf("%d %lf", &base, &exponent); // of the compiler completely ignoring the order
+    printf("%Lf", powerTo(exponent, base)); // of the functions printf and scanf. 
+}                                           // Such is life in C.
--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 
 // TODO learn to replace namespaces
 int main(){
-    ldouble exponent, base = 0;
-    cout << "enter an exponent and a base: ";
+    int exponent = 0;
+    ldouble base = 0;
+    cout << "Enter a base and an exponent: ";
     cin >> exponent >> base;
     cout << powerTo(exponent, base);
 }
